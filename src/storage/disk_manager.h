acbf3c8475a3f89a932799bca41b5c706f1baa77
Andrei Roud <aroud.by@gmail.com>
2026-05-12 02:57:07 +0200

    feat(disk): define storage API

--- src/storage/disk_manager.h
+++ src/storage/disk_manager.h
@@ -1,4 +1,31 @@
 #ifndef BUFFER_MANAGER_SRC_STORAGE_DISK_MANAGER_H_
 #define BUFFER_MANAGER_SRC_STORAGE_DISK_MANAGER_H_
 
+#include <filesystem>
+#include <fstream>
+
+#include "buffer_manager/page.h"
+
+namespace buffer_manager {
+
+class DiskManager final {
+ public:
+  explicit DiskManager(const std::filesystem::path& path);
+  ~DiskManager();
+
+  DiskManager(const DiskManager&) = delete;
+  DiskManager& operator=(const DiskManager&) = delete;
+
+  DiskManager(DiskManager&&) = delete;
+  DiskManager& operator=(DiskManager&&) = delete;
+
+  void ReadPage(std::uint64_t disk_offset, Page* page);
+  void WritePage(std::uint64_t disk_offset, const Page& page);
+
+ private:
+  std::fstream file_;
+};
+
+}  // namespace buffer_manager
+
 #endif  // BUFFER_MANAGER_SRC_STORAGE_DISK_MANAGER_H_
